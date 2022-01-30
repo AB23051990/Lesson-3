ab931ef787960d52088772c6361f2b2017e16c99
ABreus <AB23051990@yandex.ru>
2022-01-30 12:21:34 +0300

    HW5

---
+++ HW5/Program.cs
@@ -0,0 +1,13 @@
+﻿using GM;
+
+namespace HW
+{    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {            
+            GMenu.Menu();            
+        }
+    }
+   
+}
